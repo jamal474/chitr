261bba795d7e1d16808e3704bf2b1713d739edee
Md Shabbir Jamal <mdshabbir820141@gmail.com>
2025-12-05 23:01:06 +0530

    feat: old project init

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+class Chitr(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    default_options = {
+        "wxwidgets/*:mediactrl": True
+    }
+
+    def requirements(self):
+        self.requires("wxwidgets/3.2.8")
+
+    def generate(self):
+            deps = CMakeDeps(self)
+            deps.generate()
+            tc = CMakeToolchain(self)
+            tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include "RootFrame.h"
+#include <wx/wx.h>
+using namespace std;
+
+/**
+ * Chitr is the main entry point of the application
+ * it is inherited from wxApp class which can be considered
+ * as the application itself
+ */
+class Chitr : public wxApp {
+public:
+    virtual bool OnInit() wxOVERRIDE {
+        RootFrame *rootFrame = new RootFrame("Chitr : Media Player");
+        rootFrame->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(Chitr);
