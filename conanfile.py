2fa95d3afb4cef9c55a9775cc4678f1a48e61856
Md Shabbir Jamal <mdshabbir820141@gmail.com>
2026-09-04 23:39:43 +0530

    fix: version patch

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,12 @@ class Chitr(ConanFile):
     def requirements(self):
         self.requires("wxwidgets/3.2.8")
         self.requires("iconfontcppheaders/cci.20240620")
+        # Resolve a glib version conflict that only shows up on Linux:
+        #   gstreamer/1.22.3 (pulled in by gtk when mediactrl=True) pins glib/2.78.3
+        #   gtk / gdk-pixbuf / pango / ... accept glib/[^2.78] -> conan resolves 2.86.5
+        # Both constraints are satisfied by 2.78.3, so pin the whole graph there.
+        if self.settings.os == "Linux":
+            self.requires("glib/2.78.3", override=True)
 
     def generate(self):
             deps = CMakeDeps(self)
